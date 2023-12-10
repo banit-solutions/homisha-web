7214793d89bcc6231372e52fdefe4790432769b6
Angatia Benson <angatiabenson1@gmail.con>
2023-12-10 04:00:22 -0600

    update 2023_12_10_041241_create_managers_table.php

--- database/migrations/2023_12_10_041241_create_managers_table.php
+++ database/migrations/2023_12_10_041241_create_managers_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('managers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('county')->nullable();
+            $table->string('id_number')->unique();
+            $table->string('profile_image')->nullable();
             $table->timestamps();
         });
     }
